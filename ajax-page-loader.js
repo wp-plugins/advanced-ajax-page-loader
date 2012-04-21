aff9ae12b822a2e7c9c1a85fb692aaf6f69502e6
deano1987 <deano1987@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-21 11:21:25 +0000

    git-svn-id: https://plugins.svn.wordpress.org/advanced-ajax-page-loader/tags/2.5.4@534443 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ajax-page-loader.js
@@ -0,0 +1,259 @@
+/*
+Plugin Name: Advanced AJAX Page Loader
+Version: 2.5.4
+Plugin URI: http://software.resplace.net/WordPress/AjaxPageLoader.php
+Description: Load pages within blog without reloading page, shows loading bar and updates the browsers URL so that the user can bookmark or share the url as if they had loaded a page normally. Also updates there history so they have a track of there browsing habbits on your blog!
+Author URI: http://dean.resplace.net
+Author: Dean Williams
+*/
+
+//Set this to true if your getting some javascript problems
+var DocReadyReload = false;
+
+//Dont mess with these...
+var isWorking = false;
+var startAjax = false;
+var searchAction = null;
+var ua = $.browser;
+
+
+//The holy grail...
+$(document).ready(function() {
+	AAPL_loadPageInit("");
+});
+
+
+window.onpopstate = function(event) {
+	//We now have a smart multi-ignore feature controlled by the admin panel
+	if (startAjax === true && AAPL_check_ignore(document.location.toString()) == true) {	
+		AAPL_loadPage(document.location.toString(),1);
+	}
+};
+
+function AAPL_loadPageInit(scope){
+	$(scope+"a").click(function(event){
+		//if its not an admin url, or doesnt contain #
+		if (this.href.indexOf(AAPLhome) >= 0 && AAPL_check_ignore(this.href) == true){
+			// stop default behaviour
+			event.preventDefault();
+
+			// remove click border
+			this.blur();
+
+			// get caption: either title or name attribute
+			var caption = this.title || this.name || "";
+
+			// get rel attribute for image groups
+			var group = this.rel || false;
+
+			// highlight the current menu item
+			$('ul.menu li').each(function() {
+				$(this).removeClass('current-menu-item');
+			});
+			$(this).parents('li').addClass('current-menu-item');
+			
+			
+			//menu item changer if your using "Suffusion" theme
+			/*
+			$('ul.sf-menu li').each(function() {
+				$(this).removeClass('current_page_item');
+				$(this).find('a').removeClass('current');
+			});
+			$(this).parents('li').addClass('current_page_item');
+			$('li.current_page_item a').addClass('current');
+			*/
+
+			// display the box for the elements href
+			AAPL_loadPage(this.href);
+		}
+	});
+  
+	if (scope == "") { 
+		if ($('#searchform').attr("action")) {
+			//Get the current action so we know where to submit to
+			searchAction = $('#searchform').attr("action");
+
+			//bind our code to search submit, now we can load everything through ajax :)
+			$('#searchform').name = 'searchform';
+			$('#searchform').attr("action", "javascript:submitSearch('s='+document.getElementById('s').value)");
+		} else {
+			if (AAPL_warnings == true) {
+				alert("Could not bind to search form...\nCould not find element with id='searchform' or attribute 'action' missing.");
+			}
+		}
+	}
+}
+
+function AAPL_loadPage(url, push, getData){
+
+	if (!isWorking){
+		scroll(0,0);
+		isWorking = true;
+		
+		//enable onpopstate
+		startAjax = true;
+		
+		//AJAX Load page and update address bar url! :)
+		//get domain name...
+		nohttp = url.replace("http://","").replace("https://","");
+		firstsla = nohttp.indexOf("/");
+		pathpos = url.indexOf(nohttp);
+		path = url.substring(pathpos + firstsla);
+		
+		//Only do a history state if clicked on the page.
+		if (push != 1) {
+			//TODO: implement a method for IE
+			if (typeof window.history.pushState == "function") {
+				var stateObj = { foo: 1000 + Math.random()*1001 };
+				history.pushState(stateObj, "ajax page loaded...", path);
+			} else {
+				if (AAPL_warnings == true) {
+					alert("'pushState' method not supported in this browser, sorry about that!");
+				}
+			}
+		}
+		
+		if (!$('#' + AAPL_content)) {
+			if (AAPL_warnings == true) {
+				alert("Could not find content region, you need to set an ID to an element that surrounds the content on the page, make sure the 'content' variable is also set to the ID name.");
+				return false;
+			}
+		}
+		
+		//start changing the page content.
+		$('#' + AAPL_content).fadeOut("slow", function() {
+			$('#' + AAPL_content).html(AAPL_loading_code);
+			$('#' + AAPL_content).fadeIn("slow", function() {
+				$.ajax({
+					type: "GET",
+					url: url,
+					data: getData,
+					cache: true,
+					dataType: "html",
+					success: function(data) {
+						isWorking = false;
+						//get title attribute
+						datax = data.split('<title>');
+						titlesx = data.split('</title>');
+						
+						if (datax.length == 2 || titlesx.length == 2) {
+							data = data.split('<title>')[1];
+							titles = data.split('</title>')[0];
+							
+							//set the title?
+							//TODO: this still doesnt set the title in the history list (atleast in chrome...) more research required here.
+							document.title =  $('<div>').text(titles).html();
+						} else {
+							if (AAPL_warnings == true) {
+								alert("You seem to have more than one <title> tag on the page, this is going to cause some major problems so page title changing is disabled.");
+							}
+						}
+						
+						//get content
+						data = data.split('id="' + AAPL_content + '"')[1];
+						data = data.substring(data.indexOf('>') + 1);
+						var depth = 1;
+						var output = '';
+						
+						while(depth > 0) {
+							temp = data.split('</div>')[0];
+							
+							//count occurrences
+							i = 0;
+							pos = temp.indexOf("<div");
+							while (pos != -1) {
+								i++;
+								pos = temp.indexOf("<div", pos + 1);
+							}
+							//end count
+							depth=depth+i-1;
+							output=output+data.split('</div>')[0] + '</div>';
+							data = data.substring(data.indexOf('</div>') + 6);
+						}
+
+						//put the resulting html back into the page!
+						$('#' + AAPL_content).html(output);
+
+						//move content area so we cant see it.
+						$('#' + AAPL_content).css("position", "absolute");
+						$('#' + AAPL_content).css("left", "20000px");
+
+						//show the content area
+						$('#' + AAPL_content).show();
+
+						//recall loader so that new URLS are captured.
+						AAPL_loadPageInit("#" + AAPL_content + " ");
+						
+						if (DocReadyReload == true) {
+							$(document).trigger("ready");
+						}
+						
+						///////////////////////////////////////////
+						//  WE HAVE AN ADMIN PAGE NOW - GO THERE //
+						///////////////////////////////////////////
+						
+						AAPL_reload_code();		
+						
+						//How to re-call the jScrollPane...
+						//$('.scroll-pane').jScrollPane();
+						
+						//How to re-call nivoSlider
+						//$('#slider').nivoSlider({
+						//	pauseTime:3000,
+						//	effect:'boxRandom',
+						//	animSpeed:700,
+						//	directionNav:true,
+						//	controlNav: false
+						//});
+
+						//now hide it again and put the position back!
+						$('#' + AAPL_content).hide();
+						$('#' + AAPL_content).css("position", "");
+						$('#' + AAPL_content).css("left", "");
+
+						$('#' + AAPL_content).fadeIn("slow", function() {
+							//errmmm... Well isnt this embarrasing... Nothing to do here :s Kinda makes you think why is there a function attatched in the first place...
+							return true;
+							//Ahhh. See what I did there? NO ITS NOT POINTLESS... NO! ... ummm ok it kinda is :(
+							//.
+							//..
+							//...
+							//....
+							//Funny though ;)
+						});
+					},
+					error: function(jqXHR, textStatus, errorThrown) {
+						//Would append this, but would not be good if this fired more than once!!
+						isWorking = false;
+						document.title = "Error loading requested page!";
+						$('#' + AAPL_content).html(AAPL_loading_error_code);
+					}
+				});
+			});
+		});
+	}
+}
+
+function submitSearch(param){
+	if (!isWorking){
+		AAPL_loadPage(searchAction, 0, param);
+	}
+}
+
+function AAPL_check_ignore(url) {
+	for (var i in AAPL_ignore) {
+		if (url.indexOf(AAPL_ignore[i]) >= 0) {
+			return false;
+		}
+	}
+	
+	//WHOA!! Lets fuck IE7 and IE8 off because they are fucking shite!
+	if ( ua.msie && (ua.version.slice(0,1) == "8" || ua.version.slice(0,1) == "7") ) {
+		if (AAPL_warnings == true) {
+			alert("Unfortunately there is a bug in IE7 and IE8 which affects the renderer, it's called the peakaboo bug. So we have disabled this plugin.");
+		}
+		return false;
+	}
+	
+	return true;
+}
---
+++ options.php
@@ -0,0 +1,147 @@
+<?php
+$uploads = wp_upload_dir();
+$GLOBALS['AAPLimages'] = $uploads['basedir'] . '/AAPL';
+$GLOBALS['AAPLimagesurl'] = $uploads['baseurl'] . '/AAPL';
+
+if (get_option('AAPL_upload_error')) {
+	echo get_option('AAPL_upload_error');
+}
+?>
+<div class="wrap">
+	<h2>Advanced Ajax Page Loader</h2>
+	
+	<div style="border:1px solid #720921;color:#720921; background-color:#f9dbe1 ;padding:5px;margin-top:10px;margin-bottom:20px;">
+		<div style="float:right">
+			<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+				<input type="hidden" name="cmd" value="_s-xclick">
+				<input type="hidden" name="hosted_button_id" value="3P6VT6B5EBVCA">
+				<input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online.">
+				<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+			</form>
+		</div>
+		I'm working really hard to provide extremely useful functionality for this plugin, over the coming months this plugin will be packed with more and more features to make your life easier!
+		<br>
+		If your using this on your site and it has saved you time/money and/or just made your site FREAKIN AWESOME, or you are using this plugin in a commercial project, think about the time and
+		effort put into this plugin for free so that life is easier for you, <b>consider a donation!</b>
+	</div>
+
+	<form method="post" action="options.php" enctype="multipart/form-data">
+		<?php wp_nonce_field('update-options'); ?>
+		<?php settings_fields('AAPL'); ?>
+
+		<table class="form-table">
+
+			<tr valign="top">
+				<th scope="row">Content Element ID:</th>
+				<td><input type="text" name="AAPL_content_id" value="<?php echo get_option('AAPL_content_id'); ?>" style="width:200px;" /></td>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row">Loading Image</th>
+				<td>
+			
+					<div style="float:left">
+						Uploaded:<br>
+						<script type="text/javascript">
+							function AAPLchangeimg(that) {
+								document.getElementById('currentl').src="<?php echo $GLOBALS['AAPLimagesurl'] . '/loaders/'; ?>" + that.options[that.selectedIndex].value;
+								//$('#currentl').attr('src','' + $('#selectl option:selected').val());
+							}
+						</script>
+						<select id="selectl" name="AAPL_loading_img" onchange="AAPLchangeimg(this);" style="width:200px;">
+							<?php
+							$files = scandir($GLOBALS['AAPLimages'] . '/loaders');
+							
+							foreach ($files as $file) {
+								if ($file != "." && $file != "..") {
+									AAPL_rcopy("$src/$file", "$dst/$file");
+									?>
+									<option value="<?php echo $file; ?>" <?php if (strcmp(get_option('AAPL_loading_img'), $file)==0) { echo ' SELECTED="SELECTED" ';} ?>><?php echo $file; ?></option>
+									<?php
+								}
+							}
+							?>
+						</select><br><br>
+						Upload:<br>
+						<input type="file" name="AAPLuploadloader" style="width:200px;">
+					</div>
+					
+					<div style="float:left; border:1px dashed #464646; padding:10px; margin-left:10px;width:170px;height:130px;">
+						Selected:<br>
+						<div style="width:180px;height:120px;overflow:auto;">
+							<img id="currentl" src="<?php echo $GLOBALS['AAPLimagesurl'] . '/loaders/' . get_option('AAPL_loading_img'); ?>" alt="" title="" />
+						</div>
+					</div>
+					
+					<div style="clear:both"></div>
+				</td>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row">Loading Layout</th>
+				<td>
+					Here you can change the HTML of the loading content, there are some special tags you can use:<br>
+					<i>{loader} - the loader image as defined above.</i>
+					<br><br>
+					<textarea name="AAPL_loading_code" style="width:100%;height:180px"><?php echo get_option('AAPL_loading_code'); ?></textarea>
+				</td>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row">Error Loading Layout</th>
+				<td>
+					Here you can change the HTML of the loading content when an error occurs, there are some special tags you can use:<br>
+					<i>{loader} - the loader image as defined above.</i>
+					<br><br>
+					<textarea name="AAPL_loading_error_code" style="width:100%;height:180px"><?php echo get_option('AAPL_loading_error_code'); ?></textarea>
+				</td>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row">Reload Code</th>
+				<td>
+					Drop any reload code you need below, if you need any help with this then please refer to the '<a href="http://software.resplace.net/WordPress/AjaxPageLoader.php" target="_blank">useful reload codes</a>', or <a href="http://wordpress.org/extend/plugins/advanced-ajax-page-loader/" target="_blank">ask for help on the WordPress forum</a>.<br>
+					<i>Make sure function AAPL_reload_code() { } isnt around the code (if you upgraded from 2.5.0)</i>
+					<br><br>
+					<textarea name="AAPL_reload_code" style="width:100%;height:250px"><?php echo get_option('AAPL_reload_code'); ?></textarea>
+				</td>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row">HREF Ignore List</th>
+				<td>
+					If you need to ignore certain urls then this is useful for you, insert the full URL, or part of a URL to capture a pattern of links, seperate each with a comma.<br>
+					<i>default: "#, /wp-, .pdf, .zip, .rar".</i>
+					<br><br>
+					<textarea name="AAPL_ignore_list" style="width:100%;height:50px"><?php echo get_option('AAPL_ignore_list'); ?></textarea>
+				</td>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row">Plugin Debug</th>
+				<td>
+					Fiddle with these options if your having problems...<br><br>
+					<input id="AAPL_js_debug" type="checkbox" name="AAPL_js_debug" value="true" <?php if (strcmp(get_option('AAPL_js_debug'), "true")==0) { echo ' CHECKED="CHECKED" ';} ?> /><label for="AAPL_js_debug">Enable JavaScript Debug Messages.</label><br>
+					<input id="AAPL_jquery_check" type="checkbox" name="AAPL_jquery_check" value="true" <?php if (strcmp(get_option('AAPL_jquery_check'), "true")==0) { echo ' CHECKED="CHECKED" ';} ?> /><label for="AAPL_js_debug">Enable jQuery check.</label>
+				</td>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row">Link to us</th>
+				<td>
+					Show your support for AAPL and link back to us :)<br><br>
+					<input id="AAPL_sponsor" type="checkbox" name="AAPL_sponsor" value="true" <?php if (strcmp(get_option('AAPL_sponsor'), "true")==0) { echo ' CHECKED="CHECKED" ';} ?> /><label for="AAPL_js_debug">Enable footer link.</label><br>
+					</td>
+			</tr>
+
+		</table>
+
+		<input type="hidden" name="action" value="update" />
+		<input type="hidden" name="AAPL_version" value="<?php echo get_option('AAPL_version'); ?>" />
+
+		<p class="submit">
+			<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+		</p>
+
+	</form>
+</div>
